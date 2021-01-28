595c1f91c36a947b53a2f1a51351b8fb2da4453c
yifan237 <65146630+yifan237@users.noreply.github.com>
2021-01-28 21:25:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 软件包地址：
+git clone https://github.com/281677160/openwrt-package
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
